52044e12e434f74b9cf43d69e9ed6ef9cbbcc0b1
Diego <Diego@Diego-PC>
2015-11-14 21:32:05 -0500

    Test in FRDNE and Fixes

--- src/control/MainControl.java
+++ src/control/MainControl.java
@@ -1,8 +1,70 @@
 package control;
 
 public class MainControl {
+	
 	public static void main(String[] args){
         MainViewControl mainViewControl = new MainViewControl();
         mainViewControl.setInitialConditions();
     }
+	
+	// Test Code - Uncommet to do test about FRDNE Layout
+	/*
+	private static java.io.BufferedReader buffRead;
+
+	public static void main(String[] args){
+		java.io.File file1 = new java.io.File("C:\\Users\\Diego\\Desktop\\Test\\FRDNE301015063645.txt");
+		java.io.File file2 = new java.io.File("C:\\Users\\Diego\\Desktop\\Test\\FRDNE301015093738.txt");
+		java.io.File file3 = new java.io.File("C:\\Users\\Diego\\Desktop\\Test\\FRDNE301015060616.txt");
+		try {
+			//printText(file1);
+			//printText(file2);
+			printText(file3);
+		} catch (java.IO.IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printText(java.io.File file) throws java.io.IOException{
+		buffRead = new java.io.BufferedReader(new java.io.FileReader(file));
+		String currentLine;
+		while((currentLine = buffRead.readLine()) !=null){
+			if(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER).equals("000")){
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER));
+				System.out.println(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_CHECK_ID, entity.FRDNELayoutConstants.END_CHECK_ID));
+			}else
+			if(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER).equals("100")){
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER)+"testtesttesttest");
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.PARTNER_TYPE_INDEX, entity.FRDNELayoutConstants.PARTNER_TYPE_INDEX+1));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_ACTION_TYPE, entity.FRDNELayoutConstants.END_ACTION_TYPE));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_NAME, entity.FRDNELayoutConstants.END_PARTNER_NAME));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_ADDRESS, entity.FRDNELayoutConstants.END_PARTNER_ADDRESS));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_COUNTY, entity.FRDNELayoutConstants.END_PARTNER_COUNTY));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_CITY, entity.FRDNELayoutConstants.END_PARTNER_CITY));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_STATE, entity.FRDNELayoutConstants.END_PARTNER_STATE));
+				System.out.println(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_PARTNER_CEP, entity.FRDNELayoutConstants.END_PARTNER_CEP));
+			}else
+			if(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER).equals("140")){
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.DOCUMENT_TYPE_INDEX,entity.FRDNELayoutConstants.DOCUMENT_TYPE_INDEX+1));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_TRANSMITTER_CNPJ, entity.FRDNELayoutConstants.END_TRANSMITTER_CNPJ));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_FISCAL_NOTE, entity.FRDNELayoutConstants.END_FISCAL_NOTE));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_DOCUMENT_SERIE, entity.FRDNELayoutConstants.END_DOCUMENT_SERIE));
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_DOCUMENT_EMISSION_DATE, entity.FRDNELayoutConstants.END_DOCUMENT_EMISSION_DATE));
+				System.out.println(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_DOCUMENT_SHIPMENT_DATE, entity.FRDNELayoutConstants.END_DOCUMENT_SHIPMENT_DATE));
+			}else
+			if(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER).equals("150")){
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER)+" ");
+				System.out.print(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_MATERIAL_DESCRIPTION, entity.FRDNELayoutConstants.END_MATERIAL_DESCRIPTION));
+				System.out.println(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_MATERIAL_CODE, entity.FRDNELayoutConstants.END_MATERIAL_CODE));;
+			}else
+			if(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER).equals("160")){
+				System.out.println(currentLine.substring(entity.FRDNELayoutConstants.BEGIN_REGISTER, entity.FRDNELayoutConstants.END_REGISTER) + "OK!");
+			}else{
+				System.out.println("ERROR");
+			}
+			
+		}
+	}
+	
+	*/
 }
--- src/entity/FRDNELayoutConstants.java
+++ src/entity/FRDNELayoutConstants.java
@@ -12,8 +12,8 @@ public class FRDNELayoutConstants {
 	
 	//	Register Types
 	
-	public final static int REGISTER_BEGIN = 0;
-	public final static int REGISTER_END = 2;
+	public final static int BEGIN_REGISTER = 0;
+	public final static int END_REGISTER = 3;
 	public final static int OPERATION_INDEX = 3;
     
     //		Identificação do arquivo de interface
@@ -36,7 +36,7 @@ public class FRDNELayoutConstants {
     //  	Identificação do arquivo de interface
     
     public final static int BEGIN_CHECK_ID = 3;
-    public final static int END_CHECK_ID = 5;
+    public final static int END_CHECK_ID = 9;
     
     public final static String CHECK_ID = "IntDNE";
     
@@ -44,43 +44,43 @@ public class FRDNELayoutConstants {
     
     public final static int PARTNER_TYPE_INDEX = 19;
     public final static int BEGIN_ACTION_TYPE = 20;
-    public final static int END_ACTION_TYPE = 33;
+    public final static int END_ACTION_TYPE = 34;
     public final static int BEGIN_PARTNER_NAME = 34;
-    public final static int END_PARTNER_NAME = 73;
+    public final static int END_PARTNER_NAME = 74;
     public final static int BEGIN_PARTNER_ADDRESS = 74;
-    public final static int END_PARTNER_ADDRESS = 123;
+    public final static int END_PARTNER_ADDRESS = 124;
     public final static int BEGIN_PARTNER_COUNTY = 124;
-    public final static int END_PARTNER_COUNTY = 163;
+    public final static int END_PARTNER_COUNTY = 164;
     public final static int BEGIN_PARTNER_CITY = 164;
-    public final static int END_PARTNER_CITY = 193;
+    public final static int END_PARTNER_CITY = 194;
     public final static int BEGIN_PARTNER_STATE = 194;
-    public final static int END_PARTNER_STATE = 196;
+    public final static int END_PARTNER_STATE = 197;
     public final static int BEGIN_PARTNER_CEP = 197;
-    public final static int END_PARTNER_CEP = 203;
+    public final static int END_PARTNER_CEP = 204;
     
     //  	Dados do cabeçalho do documento de transporte (DNE) utilizado pela transportadora para o calculo do frete
     
-    public final static int DOCUMENT_TYPE_INDEX = 4;
-    public final static int BEGIN_TRANSMITTER_CNPJ = 5;
-    public final static int END_TRANSMITTER_CNPJ = 19;
+    public final static int DOCUMENT_TYPE_INDEX = 3;
+    public final static int BEGIN_TRANSMITTER_CNPJ = 4;
+    public final static int END_TRANSMITTER_CNPJ = 20;
     public final static int BEGIN_FISCAL_NOTE = 20;
-    public final static int END_FISCAL_NOTE = 31;
+    public final static int END_FISCAL_NOTE = 32;
     public final static int BEGIN_DOCUMENT_SERIE = 32;
-    public final static int END_DOCUMENT_SERIE = 34;
+    public final static int END_DOCUMENT_SERIE = 35;
     public final static int BEGIN_DOCUMENT_EMISSION_DATE = 35;
-    public final static int END_DOCUMENT_EMISSION_DATE = 44;
+    public final static int END_DOCUMENT_EMISSION_DATE = 45;
     public final static int BEGIN_DOCUMENT_SHIPMENT_DATE = 45;
-    public final static int END_DOCUMENT_SHIPMENT_DATE = 54;
+    public final static int END_DOCUMENT_SHIPMENT_DATE = 55;
     public final static int ACTION_TYPE_INDEX = 55;
     public final static int BEGIN_RECEIEVER_CNPJ = 56;
-    public final static int END_RECEIEVER_CNPJ = 70;
+    public final static int END_RECEIEVER_CNPJ = 71;
     
     //		Dados cadastrais dos materiais
     
     public final static int BEGIN_MATERIAL_DESCRIPTION = 4;
-    public final static int END_MATERIAL_DESCRIPTION = 43;
+    public final static int END_MATERIAL_DESCRIPTION = 44;
     public final static int BEGIN_MATERIAL_CODE = 44;
-    public final static int END_MATERIAL_CODE = 64;
+    public final static int END_MATERIAL_CODE = 65;
     
     //		Dados dos itens do documento de transporte utilizado pela transportadora.
     
@@ -103,5 +103,4 @@ public class FRDNELayoutConstants {
     public final static int START_DATE_2 = 45;
     public final static int END_DATE_2 = 54;
 
-    
 }
